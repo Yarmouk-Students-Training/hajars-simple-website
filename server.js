f45c41522444c3cea42715160dab3cc9c4ca7598
Hajar Alsharea <2018901125@ses.yu.edu.jo>
2021-03-19 13:57:39 +0200

    version1

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const http=require('http');
 const fs =require('fs');
+const _=require('lodash');
+
 const server=http.createServer((req,res)=> {
     //console.log('request made');
     console.log(req.url,req.method);
 
+    //lodash
+     const num =_.random(0,20);
+     console.log(num);
+    
+     const greet =  _.once(()=>{
+        console.log('hello');
+    });
+    greet();
+    greet();
+     
+     
     // if i want to sendback text/plain
     /* res.setHeader('content-type', 'text/plain');
     res.write('hello hajar');
@@ -17,7 +30,31 @@ const server=http.createServer((req,res)=> {
     
     //if i want to sendback external html file
     res.setHeader('content-type', 'text/html');
-    fs.readFile('./views/index2.html',(err,data) =>{
+    let path= './views/';
+    switch(req.url){
+        case '/':
+            path +='index2.html';
+            res.statusCode =200;
+            break;
+        case'/about': 
+            path +='about.html';
+            res.statusCode =200;
+            break;
+        case'/about-blah': 
+            res.statusCode =301;
+            res.setHeader('Location','/about');
+            res.end();
+            
+            break;   
+      
+        default:
+            path +='404.html';
+            res.statusCode =404;
+            break;
+     
+    }
+    //fs.readFile('./views/index2.html',(err,data) =>{ 
+    fs.readFile(path,(err,data) =>{
         if(err){
             console.log(err); 
             res.end();
@@ -30,8 +67,8 @@ const server=http.createServer((req,res)=> {
     });
 });
 
-server.listen(3000,'localhost',()=> {
-    console.log('listening for request on port 3000');
+server.listen(5051,'localhost',()=> {
+    console.log('listening for request on port 5051');
     
 
 })
